f1d3984f716cc782df8cce7c8e4f5ccc9875757f
Vicer <0oVicero0@users.noreply.github.com>
2018-10-30 16:24:19 +0800

    Update list.php

--- view/nexmoe/list.php
+++ view/nexmoe/list.php
@@ -72,15 +72,6 @@ function file_ico($item){
 <div class="mdui-row">
 	<ul class="mdui-list">
 		<li class="mdui-list-item th">
-		  <div class="mdui-left mdui-text-left">
-		    <label class="mdui-switch">
-		      <span class="mdui-chip-title">看图模式</span>
-		        <input type="checkbox" id="image_view" <?php if ($_COOKIE["image_mode"] == "1") {echo "checked";} ?>>
-		      <i class="mdui-switch-icon"></i>
-		    </label>
-		  </div>
-		</li>
-		<li class="mdui-list-item th">
 		  <div class="mdui-col-xs-12 mdui-col-sm-7">文件 <i class="mdui-icon material-icons icon-sort" data-sort="name" data-order="downward">expand_more</i></div>
 		  <div class="mdui-col-sm-3 mdui-text-right">修改时间 <i class="mdui-icon material-icons icon-sort" data-sort="date" data-order="downward">expand_more</i></div>
 		  <div class="mdui-col-sm-2 mdui-text-right">大小 <i class="mdui-icon material-icons icon-sort" data-sort="size" data-order="downward">expand_more</i></div>
@@ -129,18 +120,18 @@ function file_ico($item){
 			<?php endif;?>
 		<?php endforeach;?>
 
-		  <?php if($zongye > 1 ):?>
+		  <?php if($totalpage > 1 ):?>
 		  <li class="mdui-list-item th">
 		    <div class="mdui-col-sm-6 mdui-left mdui-text-left">
 		      <?php if(($page-1) >= 1 ):?>
-		        <a href="/?<?php e($fullpath) ?>.page-<?php e($page-1) ?>/" class="mdui-btn mdui-btn-raised">上一页</a>
+		        <a href="<?php e($root) ?>?<?php e($path) ?>.page-<?php e($page-1) ?>/" class="mdui-btn mdui-btn-raised">上一页</a>
 		      <?php endif;?>
-		      <?php if(($page+1) <= $zongye ):?>
-		        <a href="/?<?php e($fullpath) ?>.page-<?php e($page-1) ?>/" class="mdui-btn mdui-btn-raised">下一页</a>
+		      <?php if(($page+1) <= $totalpage ):?>
+		        <a href="<?php e($root) ?>?<?php e($path) ?>.page-<?php e($page+1) ?>/" class="mdui-btn mdui-btn-raised">下一页</a>
 		      <?php endif;?>
 		    </div>
 		    <div class="mdui-col-sm-6 mdui-right mdui-text-right">
-		      <div class="mdui-right mdui-text-right"><span class="mdui-chip-title">Page: <?php e($page);?>/<?php e($zongye);?></span></div>
+		      <div class="mdui-right mdui-text-right"><span class="mdui-chip-title">Page: <?php e($page);?>/<?php e($totalpage);?></span></div>
 		    </div>
 		  </li>
 		  <?php endif;?>
